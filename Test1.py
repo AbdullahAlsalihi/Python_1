b03c8b120bfdbfd85e9e69389a9698ce38719015
Abdullah Alsalihi <abdullahalsalihi0@gmail.com>
2024-07-29 10:58:13 -0400

    Add files via upload
    Adding my 2 test python projects to GitHub

---
+++ Test1.py
@@ -0,0 +1,6 @@
+size_in_bytes = 1000202273280  # Replace this value with the actual size obtained
+
+# Convert bytes to gigabytes
+size_in_gb = size_in_bytes / (1024 ** 3)
+
+print("Size in GB:", size_in_gb)
